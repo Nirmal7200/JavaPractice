1e2d55a4f706eeef468ae57b41aeaf7ad68ce543
nirmal <nirmalmanoharan1993@gmail.com>
2026-03-23 01:40:13 +0530

    Predicate Management problem 3

---
+++ functionalProgramming/src/predicate/management/Employee.java
@@ -0,0 +1,56 @@
+package predicate.management;
+
+public class Employee {
+
+
+    String name;
+    String designation;
+    double salary;
+    String city;
+
+
+    public Employee(String name, String designation, double salary, String city){
+        this.name=name;
+        this.designation =designation;
+        this.salary=salary;
+        this.city=city;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public void setName(String name){
+        this.name = name;
+    }
+
+    public String getDesignation(){
+        return designation;
+    }
+
+    public void setDesignation(String designation){
+        this.designation = designation;
+    }
+
+    public double getSalary(){
+        return salary;
+    }
+
+    public void setSalary(double salary){
+        this.salary = salary;
+    }
+
+    public String getCity(){
+        return city;
+    }
+
+    public void setCity(String city){
+        this.city = city;
+    }
+
+    @Override
+    public String toString(){
+        String s = String.format("(%s,%s,%.2f,%s)",name,designation,salary,city);
+        return s;
+    }
+}
