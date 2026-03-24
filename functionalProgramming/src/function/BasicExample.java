cf11863ef64855c17097e649ea219d19190acaa2
nirmal <nirmalmanoharan1993@gmail.com>
2026-03-24 08:08:21 +0530

    Function interface basic example

---
+++ functionalProgramming/src/function/BasicExample.java
@@ -0,0 +1,21 @@
+package function;
+
+import java.util.function.Function;
+
+public class BasicExample {
+
+
+    public static void main(String[] args) {
+        /**
+         * Function interface has only apply method
+         * Function interface using two parameters one is for input and another one is return Function<String, Long>
+         *     Function<T,R> function
+         */
+
+        //Find the length of the String using Functional interface
+
+        Function<String, Integer> f = s -> s.length();
+
+        System.out.println(f.apply("Nirmal"));
+    }
+}
