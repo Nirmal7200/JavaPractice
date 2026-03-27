d3b2a94b8d78acde3a78850be66bd154d20f4f33
nirmal <nirmalmanoharan1993@gmail.com>
2026-03-27 07:29:48 +0530

    Function and predicate interface to implement the employee salary increment

--- functionalProgramming/src/function/totalEmpMonthSal/Main.java
+++ functionalProgramming/src/function/totalEmpMonthSal/Main.java
@@ -2,6 +2,7 @@ package function.totalEmpMonthSal;
 
 import java.util.ArrayList;
 import java.util.function.Function;
+import java.util.function.Predicate;
 
 public class Main {
 
@@ -13,7 +14,7 @@ public class Main {
         employees.add(new Employee("Nirmal", 144600));
         employees.add(new Employee("Jayaganesh", 320000));
         employees.add(new Employee("Rohit", 130000));
-        employees.add(new Employee("Arun", 350000));
+        employees.add(new Employee("Arun", 120000));
 
 
         Function<ArrayList<Employee>, Double> function = emp -> {
@@ -25,6 +26,26 @@ public class Main {
             return total;
         };
 
-        System.out.println(function.apply(employees));
+        System.out.println(STR."Total salary of all employee - \{function.apply(employees)}");
+
+        Predicate<Employee> employeePredicate = emp -> emp.getSalary()<130000;
+
+       Function<Employee, Employee> function1 = e -> {
+           e.setSalary(e.getSalary() + e.getSalary() + 10000);
+
+           return e;
+       };
+
+
+       ArrayList<Employee> l2 = new ArrayList<>();
+
+       for (Employee e : employees){
+           if (employeePredicate.test(e)){
+               l2.add(function1.apply(e));
+           }
+       }
+
+        System.out.println(l2);
+
     }
 }
